7f5a1a7634af3a005c212a67e1a56cd0c096c17c
finnishfox <onefinnishfox@gmail.com>
2019-04-23 13:35:20 +0200

    deploy docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const gulp = require('gulp'),
     csso = require('gulp-csso'),
     autoprefixer = require('gulp-autoprefixer');
 
-const outFolder = 'dist';
+const outFolder = 'docs';
 
 
 gulp.task('connect', () =>
